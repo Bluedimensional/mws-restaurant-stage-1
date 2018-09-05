b22e72a77c333c278e060805b1a6e55276289a30
ben-blue <benjaminblue@gmail.com>
2018-09-05 17:41:39 -0500

    updated file path typos.

--- sw.js
+++ sw.js
@@ -3,25 +3,26 @@ importScripts('/cache-polyfill.js');
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('airhorner').then(function (cache) {
+        caches.open('v4').then(function (cache) {
             return cache.addAll([
-                'index.html',
-                'restaurant.html',
+                '/',
+                '/index.html',
+                '/restaurant.html',
                 '/css/styles.css',
                 '/js/dbhelper.js',
                 '/js/main.js',
                 '/js/restaurant_info.js',
-                'images/1.jpg',
-                'images/2.jpg',
-                'images/3.jpg',
-                'images/4.jpg',
-                'images/5.jpg',
-                'images/6.jpg',
-                'images/7.jpg',
-                'images/8.jpg',
-                'images/9.jpg',
-                'images/10.jpg',
-                'data/restaurants.json'
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/data/restaurants.json'
             ]);
         })
     );
